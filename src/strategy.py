35c9d5640f36bc2b97b7be26fbfece7c863dc158
maxsmartofficial <Msmart4156@gmail.com>
2021-10-19 17:42:46 +0100

    Add Epsilon-first strategy

--- src/strategy.py
+++ src/strategy.py
@@ -86,6 +86,27 @@ class EpsilonGreedyStrategy(Strategy):
         if result:
             self.successes[page.name] += 1
             
+    def _getBestPage(self):
+        # Return best performing page
+        average = 0
+        for p in self.page_names.keys():
+            if self.totals[p] != 0:
+                mean = self.successes[p]/self.totals[p]
+            else:
+                mean = 0
+                
+            if mean > average:
+                average = mean
+                best_page = p
+                
+        # If there is no best page, or no data
+        if average == 0:
+            # Return a random page
+            return(random.choice(self.pages))
+        else:
+            return(self.page_names[best_page])
+                
+            
     def choice(self):
         # Sometimes, or if there is currently no data
         if random.random() < self.epsilon or sum(self.totals.values()) == 0:
@@ -94,26 +115,63 @@ class EpsilonGreedyStrategy(Strategy):
             
         else:
             # Return the currently best performing page
-            average = 0
-            for p in self.page_names.keys():
-                if self.totals[p] != 0:
-                    mean = self.successes[p]/self.totals[p]
-                else:
-                    mean = 0
-                    
-                if mean > average:
-                    average = mean
-                    best_page = p
-                    
-            # If there is no page that has any positive results
-            if average == 0:
-                # Return a page at random
-                return(random.choice(self.pages))
-            else:
-                # Otherwise, return the best page
-                return(self.page_names[best_page])
+            return(self._getBestPage())
+            
                     
                 
-            
-        
+class EpsilonFirstStrategy(Strategy):
+
+    def __init__(self, pages, initial):
     
+        self.name = "Epsilon-first Strategy (" + str(initial) + ")"
+    
+        self.initial = initial
+        self.total = 0
+        
+        for p in pages:
+            if not issubclass(p.__class__, src.page.Page):
+                raise Exception("Strategy must be given a list of Pages")
+                
+        self.pages = pages
+        self.page_names = {p.name: p for p in pages}
+                
+        self.totals = defaultdict(int)
+        self.successes = defaultdict(int)
+        
+    def update(self, page, result):
+    
+        self.total += 1
+        
+        self.totals[page.name] += 1
+        if result:
+            self.successes[page.name] += 1
+            
+    def _getBestPage(self):
+        # Return best performing page
+        average = 0
+        for p in self.page_names.keys():
+            if self.totals[p] != 0:
+                mean = self.successes[p]/self.totals[p]
+            else:
+                mean = 0
+                
+            if mean > average:
+                average = mean
+                best_page = p
+                
+        # If there is no best page, or no data
+        if average == 0:
+            # Return a random page
+            return(random.choice(self.pages))
+        else:
+            return(self.page_names[best_page])
+
+    def choice(self):
+        
+        if self.total < self.initial:
+            # Return random page
+            return(random.choice(self.pages))
+        else:
+            # Return best performing page
+            return(self._getBestPage())
+            
