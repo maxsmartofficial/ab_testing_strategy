fb598bc43e677f0128ba6d0d94beef6f91894c7e
maxsmartofficial <Msmart4156@gmail.com>
2021-10-19 17:50:58 +0100

    Refactor strategy

--- src/strategy.py
+++ src/strategy.py
@@ -60,32 +60,24 @@ class RandomStrategy(Strategy):
     def update(self, page, result):
         pass
         
-        
-class EpsilonGreedyStrategy(Strategy):
+
+
+class GreedyStrategy(Strategy):
+
+    def __init__(self, pages):
     
-    def __init__(self, pages, epsilon):
     
-        self.name = "Epsilon-greedy Strategy (" + str("{:0.2f}".format(epsilon)) + ")"
-        
-        self.epsilon = epsilon
-        
         for p in pages:
             if not issubclass(p.__class__, src.page.Page):
                 raise Exception("Strategy must be given a list of Pages")
-                
+        
         self.pages = pages
         self.page_names = {p.name: p for p in pages}
         
         self.totals = defaultdict(int)
         self.successes = defaultdict(int)
-        
-        
-    def update(self, page, result):
-        
-        self.totals[page.name] += 1
-        if result:
-            self.successes[page.name] += 1
-            
+    
+    
     def _getBestPage(self):
         # Return best performing page
         average = 0
@@ -105,7 +97,26 @@ class EpsilonGreedyStrategy(Strategy):
             return(random.choice(self.pages))
         else:
             return(self.page_names[best_page])
-                
+
+
+    def update(self, page, result):
+        
+        self.totals[page.name] += 1
+        if result:
+            self.successes[page.name] += 1
+        
+        
+
+class EpsilonGreedyStrategy(GreedyStrategy):
+    
+    def __init__(self, pages, epsilon):
+    
+        self.name = "Epsilon-greedy Strategy (" + str("{:0.2f}".format(epsilon)) + ")"
+        
+        self.epsilon = epsilon
+        
+        super().__init__(pages)
+            
             
     def choice(self):
         # Sometimes, or if there is currently no data
@@ -119,7 +130,7 @@ class EpsilonGreedyStrategy(Strategy):
             
                     
                 
-class EpsilonFirstStrategy(Strategy):
+class EpsilonFirstStrategy(GreedyStrategy):
 
     def __init__(self, pages, initial):
     
@@ -128,44 +139,16 @@ class EpsilonFirstStrategy(Strategy):
         self.initial = initial
         self.total = 0
         
-        for p in pages:
-            if not issubclass(p.__class__, src.page.Page):
-                raise Exception("Strategy must be given a list of Pages")
-                
-        self.pages = pages
-        self.page_names = {p.name: p for p in pages}
-                
-        self.totals = defaultdict(int)
-        self.successes = defaultdict(int)
+        super().__init__(pages)
+        
         
     def update(self, page, result):
     
+        super().update(page, result)
+    
         self.total += 1
         
-        self.totals[page.name] += 1
-        if result:
-            self.successes[page.name] += 1
             
-    def _getBestPage(self):
-        # Return best performing page
-        average = 0
-        for p in self.page_names.keys():
-            if self.totals[p] != 0:
-                mean = self.successes[p]/self.totals[p]
-            else:
-                mean = 0
-                
-            if mean > average:
-                average = mean
-                best_page = p
-                
-        # If there is no best page, or no data
-        if average == 0:
-            # Return a random page
-            return(random.choice(self.pages))
-        else:
-            return(self.page_names[best_page])
-
     def choice(self):
         
         if self.total < self.initial:
